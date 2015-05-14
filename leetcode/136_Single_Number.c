b0546fa9c744db71f2f7ab105cfcf21ee7f9dd03
邓义 <dengyi@yodo1.com>
2015-05-14 14:33:16 +0800

    104_Maximum_Depth_of_Binary_Tree.c 112_Path_Sum.c 125_Valid_Palindrome.c 136_Single_Number.c 58_Length_of_Last_Word.c 65_Valid_Number.cpp

--- leetcode/136_Single_Number.c
+++ leetcode/136_Single_Number.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int singleNumber(int* nums, int numsSize) {
     int result = 0;
     int i = 0;
@@ -10,12 +7,6 @@ int singleNumber(int* nums, int numsSize) {
     return result;
 }
 
-int main()
-{
-    int nums[] = {1,2,3,4,5,6,7,8,9,10,12,1,2,3,4,5,6,7,8,9,10};
-    printf("%d", singleNumber(nums, 21));
-    return 0;
-}
 /*
 	leetcode 136 Single Number
 	https://leetcode.com/problems/single-number/
